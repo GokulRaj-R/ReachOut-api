d4abd11612672aa2612aa2b1e9bb39197ea30806
GokulRaj-R <gokul8262@gmail.com>
2020-07-12 23:10:15 +0530

    Add database and user routes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+app.use(express.json({ extended: false }));
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Running'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.port || 4000;
+
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
